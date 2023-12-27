1677b843c33604c8d6c2794e4c3eadcb5758ca9e
tauhid97k <tauhid.coder@gmail.com>
2023-12-27 17:51:36 +0600

    prisma update and role permissions seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,59 @@
-const permissions = require('./rolePermissions')
 const prisma = require('../utils/prisma')
-const asyncHandler = require('express-async-handler')
 
-asyncHandler(async (req, res, next) => {})
+const roles = [{ name: 'admin' }, { name: 'user' }]
+const permissions = [
+  {
+    name: 'view_users',
+    group: 'users',
+  },
+  {
+    name: 'create_users',
+    group: 'users',
+  },
+  {
+    name: 'update_users',
+    group: 'users',
+  },
+  {
+    name: 'delete_users',
+    group: 'users',
+  },
+  {
+    name: 'view_posts',
+    group: 'posts',
+  },
+  {
+    name: 'create_posts',
+    group: 'posts',
+  },
+  {
+    name: 'update_posts',
+    group: 'posts',
+  },
+  {
+    name: 'delete_posts',
+    group: 'posts',
+  },
+]
+
+async function main() {
+  await prisma.$transaction([
+    prisma.roles.createMany({
+      data: roles,
+    }),
+    prisma.permissions.createMany({
+      data: permissions,
+    }),
+  ])
+}
+
+main()
+  .then(async () => {
+    console.log('Seeding was successful')
+    await prisma.$disconnect()
+  })
+  .catch(async (error) => {
+    console.log(error)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
